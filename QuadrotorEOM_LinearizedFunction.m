c5a2a66df33b7ecc23ae01b75fbdb62b06916f78
teddyklein4 <tedward.klein@gmail.com>
2025-10-27 16:00:52 -0600

    Fix indexing error in angular rate derivatives

--- QuadrotorEOM_LinearizedFunction.m
+++ QuadrotorEOM_LinearizedFunction.m
@@ -13,7 +13,7 @@ p = var(10); q = var(11); r = var(12);
 x_dot = u; y_dot = v; z_dot = w;
 phi_dot = p; theta_dot = q; psi_dot = r;
 u_dot = -g*theta; v_dot = g*phi; w_dot = deltaFc*(1/m);
-p_dot = (1/I(1))*deltaGc(1); q_dot = (1/I(2))*deltaGc(2); r_dot = (1/I(3))*deltaGc(3,3);
+p_dot = (1/I(1))*deltaGc(1); q_dot = (1/I(2))*deltaGc(2); r_dot = (1/I(3))*deltaGc(3);
 
 var_dot = [x_dot,y_dot,z_dot;phi_dot,theta_dot,psi_dot;u_dot,v_dot,w_dot;p_dot,q_dot,r_dot];
 
